770fbe8d462d0f72ba0d465dbfdd6e8fe597a238
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-02-02 19:17:30 +0600

    for variable slider added method setAttribute with -> id and slider

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-for (var i=25; i>0; i--){
+for (var i = 25; i > 0; i--) {
   let slider = document.createElement("div");
   slider.setAttribute("class", "slider animate");
+  slider.setAttribute("id", "slider" +i);
 }
