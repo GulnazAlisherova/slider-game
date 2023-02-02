3a26b025c0d0218f77403f4b0262e5e3705592b2
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-02-02 20:15:27 +0600

    formated JS document

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 for (var i = 25; i > 0; i--) {
   let slider = document.createElement("div");
   slider.setAttribute("class", "slider animate");
-  slider.setAttribute("id", "slider" +i);
+  slider.setAttribute("id", "slider" + i);
   document.getElementById("game").append(slider);
 }
 
-function stopSliding(slider){
+function stopSliding(slider) {
   var sliderCurrent = document.getElementById("slider".concat(slider));
-  var sliderAbove = document.getElementById("slider".concat(slider+1));
-  var sliderBelow = document.getElementById("slider".concat(slider-1));
+  var sliderAbove = document.getElementById("slider".concat(slider + 1));
+  var sliderBelow = document.getElementById("slider".concat(slider - 1));
   var left = window.getComputedStyle(sliderCurrent).getPropertyValue("left");
   sliderCurrent.classList.remove("animate");
   sliderCurrent.style.left = "left";
-  var width =  window.getComputedStyle(sliderCurrent).getPropertyValue("width");
+  var width = window.getComputedStyle(sliderCurrent).getPropertyValue("width");
   var leftBelow = parseInt(window.getComputedStyle(sliderBelow).getPropertyValue("left"));
 
-  var difference = left - leftBelow; 
+  var difference = left - leftBelow;
 }
+//https://github.com/TiagoCavalcante/To-Dos
