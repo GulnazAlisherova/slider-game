efcbb5d92d6a186372ddd9a60afde91ae11c8f62
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-02-02 20:37:33 +0600

    added one more if&else statements with several contents

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ for (var i = 25; i > 0; i--) {
 function stopSliding(slider) {
   var sliderCurrent = document.getElementById("slider".concat(slider));
   var sliderAbove = document.getElementById("slider".concat(slider + 1));
+  if(slider == 1){
+
+  }else{
   var sliderBelow = document.getElementById("slider".concat(slider - 1));
+}
   var left = window.getComputedStyle(sliderCurrent).getPropertyValue("left");
   sliderCurrent.classList.remove("animate");
   sliderCurrent.style.left = "left";
